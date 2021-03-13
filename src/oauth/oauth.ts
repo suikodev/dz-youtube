d3604bec68d169943809620db2105576fb5633c2
nacht <nacht42@riseup.net>
2021-03-13 16:43:33 +0800

    feat(src/oauth): add youtube oauth class

---
+++ src/oauth/oauth.ts
@@ -0,0 +1,42 @@
+export class OAuth {
+  constructor(clientId: string) {
+    [this.redirectUrl] = document.URL;
+    this.clientId = clientId;
+    this.accessToken = window.location.href
+      .split('#access_token=')[1]
+      ?.split('&')[0];
+  }
+
+  private redirectUrl: string;
+
+  private clientId: string;
+
+  private accessToken: string | undefined;
+
+  public oauthSignIn() {
+    const oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
+    const form = document.createElement('form');
+    form.setAttribute('method', 'GET');
+    form.setAttribute('action', oauth2Endpoint);
+
+    const params: Record<string, string> = {
+      client_id: this.clientId,
+      redirect_uri: this.redirectUrl,
+      response_type: 'token',
+      scope: 'https://www.googleapis.com/auth/youtube.force-ssl',
+      include_granted_scopes: 'true',
+      state: 'pass-through value',
+    };
+
+    for (const p of Object.keys(params)) {
+      const input = document.createElement('input');
+      input.setAttribute('type', 'hidden');
+      input.setAttribute('name', p);
+      input.setAttribute('value', params[p]);
+      form.appendChild(input);
+    }
+
+    document.body.appendChild(form);
+    form.submit();
+  }
+}
