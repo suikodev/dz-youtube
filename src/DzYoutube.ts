547db7f8c3a7f9bfe8cde3f9d7a4f8e6ef9208ae
nacht <nacht42@riseup.net>
2021-03-13 17:46:08 +0800

    feat(src/dzyoutube): add button to auth by google

--- src/DzYoutube.ts
+++ src/DzYoutube.ts
@@ -1,6 +1,16 @@
 import { html, css, LitElement, property } from 'lit-element';
+import { OAuth } from './oauth';
 
 export class DzYoutube extends LitElement {
+  /**
+   *
+   */
+  constructor() {
+    super();
+    this.oauth = new OAuth(this.clientId);
+    this.isVideoUploadFormOpenning = !!this.oauth.accessToken;
+  }
+
   static styles = css`
     :host {
       justify-content: center;
@@ -9,17 +19,35 @@ export class DzYoutube extends LitElement {
     }
 
     iframe {
-      width: 80vw;
-      height: 45vw;
+      width: 40vw;
+      height: 22.5vw;
+    }
+
+    form {
+      display: flex;
+      align-items: center;
     }
   `;
 
-  @property({ type: String }) src = "https://www.youtube.com/embed/DXUAyRRkI6k";
+  @property({ type: String }) src = 'https://www.youtube.com/embed/DXUAyRRkI6k';
+
+  @property({ type: String }) clientId = '';
+
+  isVideoUploadFormOpenning: boolean;
+
+  oauth: OAuth;
 
   render() {
     return html`
       <div>
-        <iframe title="youtubeVideo" src=${this.src} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe
+          title="youtubeVideo"
+          src=${this.src}
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+        <button @click="${this.oauth.oauthSignIn}">upload video</button>
       </div>
     `;
   }
