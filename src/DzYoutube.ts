109a69bd1407722df08329c505b70ca4436fafc3
nacht <nacht42@riseup.net>
2021-03-14 16:22:03 +0800

    feat(src/dzyoutube.ts): add form to upload video

--- src/DzYoutube.ts
+++ src/DzYoutube.ts
@@ -1,16 +1,8 @@
 import { html, css, LitElement, property } from 'lit-element';
+
 import { OAuth } from './oauth';
 
 export class DzYoutube extends LitElement {
-  /**
-   *
-   */
-  constructor() {
-    super();
-    this.oauth = new OAuth(this.clientId);
-    this.isVideoUploadFormOpenning = !!this.oauth.accessToken;
-  }
-
   static styles = css`
     :host {
       justify-content: center;
@@ -25,30 +17,133 @@ export class DzYoutube extends LitElement {
 
     form {
       display: flex;
-      align-items: center;
+      flex-direction: column;
+      justify-content: center;
+      padding: 1rem;
+      width: 30vw;
+    }
+
+    input {
+      margin-bottom: 1rem;
     }
   `;
 
-  @property({ type: String }) src = 'https://www.youtube.com/embed/DXUAyRRkI6k';
+  @property({ type: String }) src = '';
 
-  @property({ type: String }) clientId = '';
+  @property({ type: String, attribute: 'client-id' }) clientId = '';
 
-  isVideoUploadFormOpenning: boolean;
+  @property({ type: String, attribute: 'client-secret' }) clientSecret = '';
 
-  oauth: OAuth;
+  title = '';
+
+  description = '';
+
+  categoryId = 0;
 
   render() {
     return html`
-      <div>
-        <iframe
-          title="youtubeVideo"
-          src=${this.src}
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
-        ></iframe>
-        <button @click="${this.oauth.oauthSignIn}">upload video</button>
-      </div>
+      <iframe
+        title="youtubeVideo"
+        src=${this.src}
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+      <form id="upload-video-form">
+        <label for="video-title">video title:</label>
+        <input type="text" id="video-title" name="video-title" value="" />
+        <label for="video-discription">video description:</label>
+        <input
+          type="text"
+          id="video-description"
+          name="video-description"
+          value=""
+        />
+        <label for="video-category">video category</label>
+        <input
+          type="number"
+          id="video-category"
+          name="video-category"
+          value="0"
+        />
+        <input
+          type="file"
+          accept="video/*"
+          id="video-file"
+          name="video-file"
+          @change="${this.handleFileSelected}"
+          style="display: none"
+        />
+        <div>
+          <button @click="${this.handleUploadButtonClick}">upload video</button>
+        </div>
+      </form>
     `;
+  }
+
+  handleUploadButtonClick(e: Event) {
+    e.preventDefault();
+    if (!OAuth.getAccessToken()) {
+      OAuth.oauthSignIn(this.clientId);
+    } else {
+      this.title =
+        (<HTMLInputElement>this.shadowRoot?.getElementById('video-title'))
+          .value || '';
+      this.description =
+        (<HTMLInputElement>this.shadowRoot?.getElementById('video-description'))
+          .value || '';
+      this.categoryId =
+        (<HTMLInputElement>this.shadowRoot?.getElementById('video-category'))
+          .valueAsNumber || 0;
+      console.log(this.title, this.description, this.categoryId);
+      if (!this.title || !this.description || !this.categoryId) {
+        alert('please fill all field.');
+        return;
+      }
+      this.shadowRoot?.getElementById('video-file')?.click();
+    }
+  }
+
+  handleFileSelected(e: Event) {
+    const { files } = e.target as HTMLInputElement;
+    if (files === null) {
+      alert('please choose a video!');
+      return;
+    }
+
+    const snippet = {
+      title: this.title,
+      description: this.description,
+      categoryId: this.categoryId,
+    };
+
+    let uploadLocation = '';
+    window
+      .fetch(
+        `https://youtube.googleapis.com/upload/youtube/v3/videos?part=snippet&key=${this.clientSecret}&uploadType=resumable`,
+        {
+          method: 'POST',
+          headers: {
+            Authorization: `Bearer ${OAuth.getAccessToken()}`,
+          },
+          body: JSON.stringify(snippet),
+        }
+      )
+      .then(resp => {
+        uploadLocation = resp.headers.get('location') || '';
+        window
+          .fetch(uploadLocation, {
+            headers: {
+              Authorization: `Bearer ${OAuth.getAccessToken()}`,
+            },
+            method: 'POST',
+            body: files[0],
+          })
+          .then(uploadedFileResp => {
+            uploadedFileResp.json().then(value => {
+              this.src = `https://youtube.com/embed/${value.id}`;
+            });
+          });
+      });
   }
 }
